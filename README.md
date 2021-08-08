a5c2f29c3a26d0b54abeee6a1d64cef2ddc1b085
hiro4295 <atemporarry42@gmail.com>
2021-08-08 10:34:01 +0530

    a

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Avvertenze
+    
+    messaging app
+    bot functionality
+    
--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from tkinter import *
 
 root = Tk()
 root.iconbitmap('./logo.ico')
+root.title("Avvertenze")
+
+
 
 root.mainloop()
