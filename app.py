69e089c056dbc6f399773fa15595c87032a047e3
hiro4295 <atemporarry42@gmail.com>
2021-08-11 23:39:42 +0530

    s

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ root.iconbitmap('./logo.ico')
 root.title("Avvertenze")
 #root.geometry("5000x5000")
 
+def enter():
+    label_message = Label(frame, text=message_input.get())
+    label_message.grid(column=0,row=1)
+
 frame = LabelFrame(root, text="Avvertenze", padx=480, pady=480)
 frame.grid(column=0,row=0,padx=5,pady=5)
 
-button_1 = Button(frame, text = "text")
-button_1.grid(column=1, row=1)
-
 message_input = Entry(frame, width=100,borderwidth=5)
 message_input.grid(column=9,row=0, columnspan=6, padx=10, pady=10)
+
+button_1 = Button(frame, text = "text",command= enter)
+button_1.grid(column=1, row=1)
+
 
 root.mainloop() 
