ef1fc420697d77a286230a5a779a2455a92d7bb4
hiro4295 <atemporarry42@gmail.com>
2021-08-12 12:03:05 +0530

    _

--- hash.py
+++ hash.py
@@ -1,9 +1,6 @@
 import json
 from hashlib import sha256
 h = sha256()
-h.update(b'ILoveDiscord666')
-with open("data.json", "r") as f: 
-    data = json.load(f)
-
+h.update(b'Hiro')
 hash = h.hexdigest()
 print(hash)
