4476821ff9c4f05132d729d82b38f0ac4f3515d4
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 11:43:39 +0530

    Added buttons for the different bots and functions to display the different pages.

--- app.py
+++ app.py
@@ -4,29 +4,38 @@ from tkinter import *
 root = Tk()
 root.iconbitmap('./logo.ico')
 root.title("swiftzy")
+selectedBot = None
 
-def enterBot() : 
+def addBot() : 
     pass
 
-userDataFrame = LabelFrame(root, text = "User Data", padx = 20, pady = 20)
-userDataFrame.grid(column=0,row=0,padx=5,pady=5)
+def selectBot(userDataFrame, botListFrame):
+    userDataFrame.destroy()
+    botListFrame.destroy()
+    Button(root, text = "revert", command = firstDisplay).grid(row = 0, column = 0)
 
-botListFrame = LabelFrame(root, text = "Bots", padx = 20, pady = 20)
-botListFrame.grid(column=0,row=1,padx=5,pady=5)
+def firstDisplay():
+    userDataFrame = LabelFrame(root, text = "User Data", padx = 20, pady = 20)
+    userDataFrame.grid(column=0,row=0,padx=5,pady=5)
 
-Label(userDataFrame, text = "Name: Hilogen                     ").grid(row = 0, column = 0)
-Label(userDataFrame, text = "Email: example@example.com").grid(row = 0, column = 1)
+    botListFrame = LabelFrame(root, text = "Bots", padx = 20, pady = 20)
+    botListFrame.grid(column=0,row=1,padx=5,pady=5)
 
-Button(botListFrame, text = "Add Bot",command=lambda: enterBot()).grid(row = 0, column = 0, padx = 140)
+    Label(userDataFrame, text = "Name: Hilogen                     ").grid(row = 0, column = 0)
+    Label(userDataFrame, text = "Email: example@example.com").grid(row = 0, column = 1)
 
-with open("config.json", "r") as f:
-    config = json.load(f)
+    Button(botListFrame, text = "Add Bot",command=lambda: addBot()).grid(row = 0, column = 0, pady = 10)
 
-botCount = len(config)
-botAddedCount = 1
+    with open("config.json", "r") as f:
+        config = json.load(f)
 
-while botAddedCount < botCount :
-    Label(botListFrame, text = config[botAddedCount]["name"]).grid(row = botAddedCount, column = 0)
-    botAddedCount += 1
+    botCount = len(config)
+    botAddedCount = 1
+
+    while botAddedCount < botCount :
+        Button(botListFrame, text = config[botAddedCount]["name"], padx = 140, command = lambda: selectBot(userDataFrame, botListFrame)).grid(row = botAddedCount, column = 0, pady = 3)
+        botAddedCount += 1
+
+firstDisplay()
 
 root.mainloop()
