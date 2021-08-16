874351627b02fa929711fa780e694429f6905fb4
BuildTools <unconfigured@null.spigotmc.org>
2021-08-16 09:44:09 +0530

    PD asked push

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ root = Tk()
 root.iconbitmap('./logo.ico')
 root.title("swiftzy")
 selectedBot = None
+loginFrame = LabelFrame(root, text = "Log in/Sign up", padx = 80, pady = 80)
+loginFrame.grid(row = 0, column = 0, padx = 2, pady = 2)
+usernameEntry = Entry(loginFrame).grid(row = 0, column = 1, padx = 3, pady = 2)
+passwordEntry = Entry(loginFrame).grid(row = 1, column = 1, padx = 3, pady = 2)
 with open("userdata.json", "r") as f:
     userdata = json.load(f)
 
@@ -16,14 +20,14 @@ def selectBot(userDataFrame, botListFrame):
     botListFrame.destroy()
     Button(root, text = "revert", command = firstDisplay).grid(row = 0, column = 0)
 
+def login(username, password):
+    print(username)
+    print(password)
+
 def loginDisplay():
-    loginFrame = LabelFrame(root, text = "Log in/Sign up", padx = 80, pady = 80)
-    loginFrame.grid(row = 0, column = 0, padx = 2, pady = 2)
     Label(loginFrame, text = "Username/Email : ").grid(row = 0, column = 0, padx = 3, pady = 2)
     Label(loginFrame, text = "Password : ").grid(row = 1, column = 0, padx = 3, pady = 2)
-    usernameEntry = Entry(loginFrame).grid(row = 0, column = 1, padx = 3, pady = 2)
-    passwordEntry = Entry(loginFrame).grid(row = 1, column = 1, padx = 3, pady = 2)
-    Button(loginFrame, text = "Log In").grid(row = 2, column = 0, padx = 3, pady = 2)
+    Button(loginFrame, text = "Log In", command = lambda: login(usernameEntry.get(), passwordEntry.get())).grid(row = 2, column = 0, padx = 3, pady = 2)
     Button(loginFrame, text = "Sign Up instead").grid(row = 2, column = 1, padx = 3, pady = 2)
 
 def firstDisplay():
